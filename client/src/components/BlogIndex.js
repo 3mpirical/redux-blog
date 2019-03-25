0f1c56538ec3c78abb46aaba9827d742e422f29c
3mpirical <3mpirical10510@gmail.com>
2019-03-25 15:14:39 -0600

    basic client folder structure created, react-router initial setup, scss initial setup, and first component files created

---
+++ client/src/components/BlogIndex.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+
+class BlogIndex extends React.Component {
+
+
+    render() {
+        return (
+            <div className="blog-index-container">
+                hello from the blog index component
+            </div>
+        )
+    }
+}
+
+
+
+export { BlogIndex };
